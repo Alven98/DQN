d63bbd57d10a45267632e7644f4b91b591fd1fc7
Alven98 <55279647+Alven98@users.noreply.github.com>
2021-08-28 22:42:19 +0800

    Add files via upload

--- training.py
+++ training.py
@@ -1,43 +1,51 @@
-from dqn import Agent
-import numpy as np
-from utils import plotLearning
 import gym
+import numpy as np
+from dueling_ddqn_torch import Agent
+from utils import plot_learning_curve
+
+from gym import wrappers
+import os
+
 
 if __name__ == '__main__':
-    env = gym.make('CartPole-v1')
+    env = gym.make('LunarLander-v2')
     n_games = 500
-    agent = Agent(alpha=0.0005, gamma=0.99, n_actions=2, epsilon=1.0, input_dims=4,
-                  mem_size=1000000, batch_size=64, epsilon_end=0.01, restart_training=True)
+    load_checkpoint = False
 
-    scores = []
-    eps_history = []
+    agent = Agent(gamma=0.99, epsilon=1.0, eps_min=0.01, eps_dec=1e-3,
+                  lr=5e-4, input_dims=[8], n_actions=4, mem_size=1000000,
+                  batch_size=64, replace=100)
+
+    if load_checkpoint:
+        agent.load_models()
+
+    filename = 'LunarLander-DuelingDDQN-Adam-lr0005-replace100.png'
+    scores, eps_history = [], []
 
     for i in range(n_games):
         done = False
         score = 0
         observation = env.reset()
+
         while not done:
             action = agent.choose_action(observation)
             observation_, reward, done, info = env.step(action)
-            env.render()
             score += reward
-            agent.remember(observation, action, reward, observation_, done)
-            observation = observation_
+            agent.store_transition(observation, action, reward, observation_, int(done))
             agent.learn()
+            observation = observation_
+            env.render()
 
-        eps_history.append(agent.epsilon)
         scores.append(score)
-
-        avg_score = np.mean(scores[max(0, i-100):(i+1)])
-        epsilon = agent.get_epsilon()
+        avg_score = np.mean(scores[-100:])
+        eps_history.append(agent.epsilon)
         print('episode ', i, ' | score %.2f' % score,
-              ' | average score %.2f' % avg_score, ' | epsilon %.2f' % epsilon)
+              ' | average score %.2f' % avg_score, ' | epsilon %.2f' % agent.epsilon)
 
         if i % 10 == 0 and i > 0:
-            agent.save_model()
+            agent.save_models()
         if i == n_games-1:
-            agent.save_model()
+            agent.save_models()
 
-    filename = 'lunarlander.png'
     x = [i+1 for i in range(n_games)]
-    plotLearning(x, scores, eps_history, filename)
+    plot_learning_curve(x, scores, eps_history, filename)
